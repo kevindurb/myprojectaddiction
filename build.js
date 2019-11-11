c641ccbd497d13bd0d0524bb9c3248afc941399a
Kevin Durbin <kevindurb@gmail.com>
2019-11-11 07:50:28 -0700

    add index.html

--- build.js
+++ build.js
@@ -2,27 +2,50 @@ const fs = require('fs').promises;
 const path = require('path');
 const ejs = require('ejs');
 const marked = require('marked');
+const frontMatter = require('front-matter');
 
 const postFilePath = x => path.join(__dirname, './posts', x);
 const distFilePath = x => path.join(__dirname, './dist', x);
 const rootFilePath = x => path.join(__dirname, x);
 const readFile = x => fs.readFile(x, { encoding: 'utf-8' });
+const writeFile = (x, y) => fs.writeFile(x, y);
 
-async function build() {
+async function buildPosts() {
   const postTemplateContent = await readFile(rootFilePath('./post.ejs'));
   const postTemplate = ejs.compile(postTemplateContent);
   const files = await fs.readdir(postFilePath(''));
 
-  await Promise.all(files.map(async (file) => {
+  return Promise.all(files.map(async (file) => {
     console.log(file);
 
     const content = await readFile(postFilePath(file));
 
-    const html = postTemplate({ content: marked(content) });
+    const {
+      attributes,
+      body,
+    } = frontMatter(content);
+    const html = postTemplate({ ...attributes, content: marked(body) });
+    const fileName = path.basename(file, '.md');
+    const outFile = `${fileName}.html`;
 
-    const name = path.basename(file, '.md');
-    return fs.writeFile(distFilePath(`${name}.html`), html);
+    await writeFile(distFilePath(outFile), html);
+
+    return {
+      ...attributes,
+      file,
+      url: `/${outFile}`,
+    };
   }));
+}
+
+async function buildIndex(posts) {
+  const templateContent = await readFile(rootFilePath('./index.ejs'));
+  const template = ejs.compile(templateContent);
+
+  return writeFile(
+    distFilePath('index.html'),
+    template({ posts }),
+  );
 }
 
 async function clean() {
@@ -33,4 +56,5 @@ async function clean() {
 
 
 clean()
-  .then(build);
+  .then(buildPosts)
+  .then(buildIndex);
--- posts/2019-11-08-first-post.md
+++ posts/2019-11-08-first-post.md
@@ -1,3 +1,8 @@
+---
+date: 2019-11-08
+title: I Built My Own Blog From Scratch
+---
+
 # I built my own blog from scratch
 
 I really like markdown. Its lets you do all kinds of cool things:
